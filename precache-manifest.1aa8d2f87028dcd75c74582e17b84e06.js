e3d27f94dc309f65cb0f86e1e5d10a6872795e41
Julia Liu <jbliu1@asu.edu>
2018-10-10 13:01:53 -0700

    Updates

---
+++ precache-manifest.1aa8d2f87028dcd75c74582e17b84e06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jbliu1/allis-react-tutorial.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c131e8f73c8a6f0bc08a",
+    "url": "/jbliu1/allis-react-tutorial.git/static/js/runtime~main.c131e8f7.js"
+  },
+  {
+    "revision": "80bcef6eb2c1605dd3b5",
+    "url": "/jbliu1/allis-react-tutorial.git/static/js/main.80bcef6e.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/jbliu1/allis-react-tutorial.git/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "80bcef6eb2c1605dd3b5",
+    "url": "/jbliu1/allis-react-tutorial.git/static/css/main.8180b719.chunk.css"
+  },
+  {
+    "revision": "76b7aaffaf778dbbf93bd34332c3eb39",
+    "url": "/jbliu1/allis-react-tutorial.git/index.html"
+  }
+];
